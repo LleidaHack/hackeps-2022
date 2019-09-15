0f45e6204ee37a9ae746e43775c36b81fdb7204c
Guillem96 <guillem.orellana@gmail.com>
2019-09-15 18:12:35 +0200

    Remove linter warnings

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,9 +1,9 @@
 import { AuthenticationService } from '../../shared/services/authentication.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { UserModel } from 'src/app/shared/models/user.model';
-import { FormControl, FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { faEnvelope, faLock, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 import { MustMatch } from 'src/app/shared/validators/sign.validator';
 
 declare var particlesJS: any;
@@ -33,106 +33,107 @@ export class BannerComponent implements OnInit {
       loginFormModalEmail: new FormControl('', Validators.compose([Validators.email, Validators.required])),
       loginFormModalPassword: new FormControl('', Validators.compose([Validators.required, Validators.minLength(8)])),
       loginFormModalConfirmPassword: new FormControl('', Validators.compose([Validators.required]))
-    },{
+    },
+    {
       validator: MustMatch('loginFormModalPassword', 'loginFormModalConfirmPassword')
     });
     particlesJS('particles-js', {
-      'particles': {
-        'number': {
-          'value': 80,
-          'density': {
-            'enable': true,
-            'value_area': 800
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
           }
         },
         color: {
           value: '#e1bee7'
         },
         shape: {
-          'type': 'circle',
-          'stroke': {
-            'width': 0,
+          type: 'circle',
+          stroke: {
+            width: 0,
           },
-          'polygon': {
-            'nb_sides': 5
+          polygon: {
+            nb_sides: 5
           },
         },
-        'opacity': {
-          'value': 0.5,
-          'random': false,
-          'anim': {
-            'enable': false,
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
           }
         },
-        'size': {
-          'value': 3,
-          'random': true,
-          'anim': {
-            'enable': false,
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
           }
         },
-        'line_linked': {
-          'enable': true,
-          'distance': 150,
-          'color': '#f3e5f5',
-          'opacity': 0.4,
-          'width': 1
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: '#f3e5f5',
+          opacity: 0.4,
+          width: 1
         },
-        'move': {
-          'enable': true,
-          'speed': 6,
-          'direction': 'none',
-          'random': false,
-          'straight': false,
-          'out_mode': 'out',
-          'bounce': false,
-          'attract': {
-            'enable': false,
-            'rotateX': 600,
-            'rotateY': 1200
+        move: {
+          enable: true,
+          speed: 6,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
           }
         }
       },
-      'interactivity': {
-        'detect_on': 'canvas',
-        'events': {
-          'onhover': {
-            'enable': true,
-            'mode': 'bubble'
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'bubble'
           },
-          'onclick': {
-            'enable': false,
-            'mode': 'push'
+          onclick: {
+            enable: false,
+            mode: 'push'
           },
-          'resize': true
+          resize: true
         },
-        'modes': {
-          'grab': {
-            'distance': 400,
-            'line_linked': {
-              'opacity': 1
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
             }
           },
-          'bubble': {
-            'distance': 400,
-            'size': 10,
-            'duration': 2,
-            'opacity': 8,
-            'speed': 3
+          bubble: {
+            distance: 400,
+            size: 10,
+            duration: 2,
+            opacity: 8,
+            speed: 3
           },
-          'repulse': {
-            'distance': 200,
-            'duration': 0.4
+          repulse: {
+            distance: 200,
+            duration: 0.4
           },
-          'push': {
-            'particles_nb': 4
+          push: {
+            particles_nb: 4
           },
-          'remove': {
-            'particles_nb': 2
+          remove: {
+            particles_nb: 2
           }
         }
       },
-      'retina_detect': true
+      retina_detect: true
     });
   }
 
@@ -149,7 +150,7 @@ export class BannerComponent implements OnInit {
   }
 
   getShowConfirmPassword() {
-    return this.showConfirmPassword
+    return this.showConfirmPassword;
   }
 
   enableSignIn() {
@@ -164,18 +165,18 @@ export class BannerComponent implements OnInit {
     return this.signIn;
   }
 
-  getModalTitle(){
+  getModalTitle() {
     return this.modalTitle;
   }
 
   enableConfirmPassword() {
     this.showConfirmPassword = true;
-    this.modalTitle = "Únete ahora!"
+    this.modalTitle = 'Únete ahora!';
   }
 
   disableConfirmPassword() {
     this.showConfirmPassword = false;
-    this.modalTitle = "Entra ahora!"
+    this.modalTitle = 'Entra ahora!';
   }
 
   public loginWithGoogle() {
@@ -191,7 +192,7 @@ export class BannerComponent implements OnInit {
   }
 
   public signInWithMailAndPass() {
-    this.auth.loginWithMailAndPassword(this.loginFormModalEmail.value, this.loginFormModalPassword.value)
+    this.auth.loginWithMailAndPassword(this.loginFormModalEmail.value, this.loginFormModalPassword.value);
   }
 
   public signUpWithMailAndPass() {
