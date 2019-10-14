a368160cc02804eb509892bf62c97721a80c8976
MrcL97 I <9719marcel@gmail.com>
2019-10-14 23:38:13 +0200

    New Calendar display-friendly size

--- src/assets/utils/pipes/titleCasePipe.ts
+++ src/assets/utils/pipes/titleCasePipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TitleCasePipe implements PipeTransform {
   transform(value: string): string {
     if (!value) return value;
-    return value[0].toUpperCase() + value.substr(1).toLowerCase();
+    return value[0].toUpperCase() + value.substr(1).toLowerCase().slice(0,2);
   }
 }
