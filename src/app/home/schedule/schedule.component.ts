285318ec9333dc8985722d730a3c747415d79dd0
Guillem96 <guillem.orellana@gmail.com>
2019-11-22 17:44:43 +0100

    Responsive schedule

--- src/app/home/schedule/schedule.component.ts
+++ src/app/home/schedule/schedule.component.ts
@@ -6,6 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.scss']
 })
 export class ScheduleComponent implements OnInit {
+  events = [
+    {
+      icon: 'fas fa-money-check',
+      date: '8:30 - 23 OCT',
+      name: 'Check-in'
+    },
+    {
+      icon: 'fas fa-play',
+      date: '10:00 - 23 OCT',
+      name: 'Finaliza check-in y empieza la ceremonia'
+    },
+    {
+      icon: 'fas fa-laptop-code',
+      date: '11:00 - 23 OCT',
+      name: 'Start hacking'
+    },
+    {
+      icon: 'fas fa-utensils',
+      date: '14:00 - 23 OCT',
+      name: 'Comida',
+    },
+    {
+      icon: 'fas fa-pizza-slice',
+      date: '20:30 - 23 OCT',
+      name: 'Cena',
+    },
+    {
+      icon: 'fas fa-cookie-bite',
+      date: '3:00 - 24 OCT',
+      name: 'Snack Time',
+    },
+    {
+      icon: 'fas fa-flag-checkered',
+      date: '11:00 - 24 OCT',
+      name: 'Finaliza el timepo de hack',
+    },
+    {
+      icon: 'fas fa-chalkboard-teacher',
+      date: '11:30 - 24 OCT',
+      name: 'Presentación de las soluciones',
+    },
+    {
+      icon: 'fas fa-trophy',
+      date: '13:00 - 24 OCT',
+      name: 'Ceremonia de clausura y entrega de premios',
+    },
+    {
+      icon: 'fas fa-drumstick-bite',
+      date: '15:00 - 24 OCT',
+      name: 'Comida y despedida',
+    },
+  ];
 
   constructor() { }
 
