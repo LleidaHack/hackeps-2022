21377fa80023b04a89ada954330d587b3472fe28
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2022-10-11 23:54:23 +0200

    added gft as sponsor, changed terms and conditions and terms and conditions page style to compel with the rest of the web page

--- src/app/terms-and-conditions/terms-and-conditions-text/terms-and-conditions-text.component.ts
+++ src/app/terms-and-conditions/terms-and-conditions-text/terms-and-conditions-text.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-terms-and-conditions-text',
   templateUrl: './terms-and-conditions-text.component.html',
+  styleUrls: ['./terms-and-conditions-text.component.scss']
 })
 export class TermsAndConditionsTextComponent implements OnInit {
 
